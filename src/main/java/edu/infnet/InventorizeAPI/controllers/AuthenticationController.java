b81abe5b35aedd2f75ea20699d45e880cb97f7e6
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-05-26 15:54:02 -0300

    Refacts authentication service and controller to follow REST principles and implements toBuilder in all entities

--- src/main/java/edu/infnet/InventorizeAPI/controllers/AuthenticationController.java
+++ src/main/java/edu/infnet/InventorizeAPI/controllers/AuthenticationController.java
@@ -2,48 +2,28 @@ package edu.infnet.InventorizeAPI.controllers;
 
 import edu.infnet.InventorizeAPI.dto.request.AuthenticationRequestDTO;
 import edu.infnet.InventorizeAPI.dto.response.AuthenticationResponseDTO;
-import edu.infnet.InventorizeAPI.entities.AuthUser;
-import edu.infnet.InventorizeAPI.exceptions.custom.RegisteredEmailException;
 import edu.infnet.InventorizeAPI.services.AuthenticationService;
-import edu.infnet.InventorizeAPI.services.auth.JwtService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
-    private final JwtService tokenService;
-    private final AuthenticationManager authenticationManager;
-    private final AuthenticationService userRegistrationService;
-
-    public AuthenticationController(JwtService tokenService, AuthenticationManager authenticationManager, AuthenticationService userRegistrationService) {
-        this.authenticationManager = authenticationManager;
-        this.tokenService = tokenService;
-        this.userRegistrationService = userRegistrationService;
-    }
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponseDTO> login(@RequestBody @Valid AuthenticationRequestDTO userData){
-        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(userData.email(), userData.password());
-        Authentication auth = authenticationManager.authenticate(usernamePassword);
+        AuthenticationResponseDTO authenticatedUser = authenticationService.authenticate(userData);
 
-        String token = tokenService.generateToken((UserDetails) auth.getPrincipal());
-        AuthUser user = userRegistrationService.findByEmail(userData.email());
-
-        return ResponseEntity.ok(AuthenticationResponseDTO.from(token, user));
+        return ResponseEntity.ok(authenticatedUser);
     }
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponseDTO> register(@RequestBody @Valid AuthenticationRequestDTO userData) {
-        if (userRegistrationService.existsByEmail(userData.email())) throw new RegisteredEmailException("Email já cadastrado");
-
-        AuthenticationResponseDTO savedUser = userRegistrationService.registerUser(userData);
+        AuthenticationResponseDTO savedUser = authenticationService.register(userData);
 
         return ResponseEntity.ok(savedUser);
     }
