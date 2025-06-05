62efd7c392eed843f30cc370a988226ce5a602c2
Bruno Martins <brunomartins@ads.fiponline.edu.br>
2025-06-05 17:44:15 -0300

    Finishes register method tests

--- src/test/java/edu/infnet/InventorizeAPI/services/AuthenticationServiceTest.java
+++ src/test/java/edu/infnet/InventorizeAPI/services/AuthenticationServiceTest.java
@@ -42,53 +42,75 @@ public class AuthenticationServiceTest {
     @InjectMocks
     private AuthenticationService authenticationService;
 
+
     @Test
-    public void shouldRegisterUserSuccessfully() {
-        AuthenticationRequestDTO userData = getAuthenticationRequest();
-        var encodedPassword = "$2a$10$eImiTMZG4ELQ2Z8z5y3jOe";
+    public void shouldAssertCorrectUserDataWhenRegistering() {
+        var authenticationRequestDTO = mockedAuthenticationRequest();
+        var mockedSavedUser = mockedAuthUser(authenticationRequestDTO.email());
+        var userCaptor = getUserCaptor();
 
-        var authUser = AuthUser.builder()
-                .id(UUID.fromString("6e399d0f-66ad-4092-97e7-1d755388959f"))
-                .email(userData.email())
-                .hashPassword(encodedPassword)
-                .roles(Set.of(Role.ROLE_USER))
-                .build();
+        when(authUserRepository.existsByEmail(authenticationRequestDTO.email())).thenReturn(false);
+        when(passwordEncoder.encode(authenticationRequestDTO.password())).thenReturn(mockedSavedUser.getHashPassword());
+        when(authUserRepository.save(any(AuthUser.class))).thenReturn(mockedSavedUser);
 
-        var userCaptor = ArgumentCaptor.forClass(AuthUser.class);
+        UserResponseDTO userResponseDTO = authenticationService.register(authenticationRequestDTO);
 
-        when(authUserRepository.existsByEmail(userData.email())).thenReturn(false);
-        when(passwordEncoder.encode(userData.password())).thenReturn(encodedPassword);
-        when(authUserRepository.save(any(AuthUser.class))).thenReturn(authUser);
-
-
-        UserResponseDTO serviceResponse = authenticationService.register(userData);
-
-
-        verify(authUserRepository).existsByEmail(userData.email());
-        verify(passwordEncoder).encode(userData.password());
         verify(authUserRepository).save(userCaptor.capture());
-        verifyNoMoreInteractions(authUserRepository, passwordEncoder);
 
-        AuthUser capturedUser = userCaptor.getValue();
-        assertEquals(authUser.getId(), serviceResponse.id(), "O id do usuário registrado deve ser igual ao esperado");
-        assertEquals(userData.email(), capturedUser.getEmail(), "O email do usuário registrado deve ser igual ao esperado");
-        assertEquals(encodedPassword, capturedUser.getHashPassword(), "A senha criptografada do usuário registrado deve ser igual à esperada");
-        assertEquals(authUser.getRoles(), capturedUser.getRoles(), "Os papéis do usuário registrado devem ser iguais aos esperados");
+        var capturedUser = userCaptor.getValue();
+
+        assertEquals(mockedSavedUser.getId(), userResponseDTO.id(), "O id do usuário registrado deve ser igual ao esperado");
+        assertEquals(capturedUser.getEmail(), userResponseDTO.email(), "O email do usuário registrado deve ser igual ao esperado");
+        assertEquals(mockedSavedUser.getHashPassword(), capturedUser.getHashPassword(), "A senha criptografada do usuário registrado deve ser igual à esperada");
+        assertEquals(mockedSavedUser.getRoles(), capturedUser.getRoles(), "Os papéis do usuário registrado devem ser iguais aos esperados");
     }
 
-//    @Test
-//    public void shouldThrowUserAlreadyRegisteredExceptionWhenEmailExists() {
-//       AuthenticationRequestDTO userData = getAuthenticationRequest();
-//
-//        when(authUserRepository.existsByEmail(userData.email())).thenReturn(true);
-//
-//        var userRegisteredException = assertThrows(UserAlreadyRegisteredException.class, () -> {
-//            authenticationService.register(userData);
-//        },"Deve lançar UserAlreadyRegisteredException quando o email já estiver cadastrado");
-//        assertEquals(userRegisteredException.getMessage(), String.format("[ EMAIL: %s ] já cadastrado", userData.email()));
-//    }
+    @Test
+    public void shouldCallCorrectMethodsWhenRegisteringUser() {
+        var authenticationRequestDTO = mockedAuthenticationRequest();
+        var mockedSavedUser = mockedAuthUser(authenticationRequestDTO.email());
 
-    private AuthenticationRequestDTO getAuthenticationRequest() {;
-        return new AuthenticationRequestDTO("test1@email.com", "password123");
+        when(authUserRepository.save(any(AuthUser.class))).thenReturn(mockedSavedUser);
+        when(authUserRepository.existsByEmail(authenticationRequestDTO.email())).thenReturn(false);
+        when(passwordEncoder.encode(authenticationRequestDTO.password())).thenReturn("$2a$10$eImiTMZG4ELQ2Z8z5y3jOe");
+
+        authenticationService.register(authenticationRequestDTO);
+
+        verify(authUserRepository).existsByEmail(authenticationRequestDTO.email());
+        verify(passwordEncoder).encode(authenticationRequestDTO.password());
+        verify(authUserRepository).save(any(AuthUser.class));
+        verifyNoMoreInteractions(authUserRepository, passwordEncoder);
+    }
+
+    @Test
+    public void shouldThrowUserAlreadyRegisteredExceptionWhenEmailExists() {
+        var userData = mockedAuthenticationRequest();
+
+        when(authUserRepository.existsByEmail(userData.email())).thenReturn(true);
+
+        var userRegisteredException = assertThrows(UserAlreadyRegisteredException.class, () -> {
+            authenticationService.register(userData);
+        }, "Deve lançar UserAlreadyRegisteredException quando o email já estiver cadastrado");
+
+        assertEquals(userRegisteredException.getMessage(), String.format("[ EMAIL: %s ] já cadastrado", userData.email()));
+    }
+
+    // Métodos auxiliares -----------------------
+
+    private AuthenticationRequestDTO mockedAuthenticationRequest() {
+        return new AuthenticationRequestDTO("test@email.com", "password123");
+    }
+
+    private AuthUser mockedAuthUser(String email) {
+        return AuthUser.builder()
+                .id(UUID.fromString("6e399d0f-66ad-4092-97e7-1d755388959f"))
+                .email(email)
+                .hashPassword("$2a$10$eImiTMZG4ELQ2Z8z5y3jOe")
+                .roles(Set.of(Role.ROLE_USER))
+                .build();
+    }
+
+    private ArgumentCaptor<AuthUser> getUserCaptor() {
+        return ArgumentCaptor.forClass(AuthUser.class);
     }
 }
