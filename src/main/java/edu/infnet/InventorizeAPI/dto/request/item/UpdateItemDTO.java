11ab9d451f529afbae8874071cb8893c32a58af2
Bruno Martins <111077073+3runoAM@users.noreply.github.com>
2025-06-09 19:08:37 -0300

    Documentation/open api (#21)
    * Adds the  Open API documentation dependency to pom.xml and configures endpoints
    
    * Renames API title in the springdoc configuration, and also renames ItemController to reflect the change
    
    * Documents ProductDTO with Open API annotations
    
    * Reafctors AuthenticationService to correctly authenticate user
    
    * Refactors Authentication Service tests to reflect changes in the authentication code
    
    * Continues documenting on dtos
    
    * Finish documenting the requestDtos
    
    * Finish documenting response dtos

--- src/main/java/edu/infnet/InventorizeAPI/dto/request/item/UpdateItemDTO.java
+++ src/main/java/edu/infnet/InventorizeAPI/dto/request/item/UpdateItemDTO.java
@@ -1,10 +1,33 @@
 package edu.infnet.InventorizeAPI.dto.request.item;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record UpdateItemDTO(
-        @NotNull @PositiveOrZero int currentQuantity,
-        @NotNull @PositiveOrZero int minimumStockLevel
-) {
-}
+        @Schema(
+                description = "A quantidade atual do item no inventário",
+                examples = {
+                        "10",
+                        "5",
+                        "1"
+                },
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @NotNull(message = "A quantidade atual do item é obrigatória neste contexto")
+        @PositiveOrZero(message = "A quantidade atual do item deve ser zero ou positiva")
+        Integer currentQuantity,
+
+        @Schema(
+                description = "O limite crítico de estoque do item",
+                examples = {
+                        "10",
+                        "5",
+                        "1"
+                },
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @NotNull(message = "O limite crítico de estoque do item é obrigatório neste contexto")
+        @PositiveOrZero(message = "O limite crítico de estoque do item deve ser zero ou positivo")
+        Integer minimumStockLevel
+) {}
--- src/main/java/edu/infnet/InventorizeAPI/dto/request/product/PatchProductDTO.java
+++ src/main/java/edu/infnet/InventorizeAPI/dto/request/product/PatchProductDTO.java
@@ -1,9 +1,31 @@
 package edu.infnet.InventorizeAPI.dto.request.product;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.hibernate.validator.constraints.Length;
 
 public record PatchProductDTO(
-        @Length(min = 2, max = 100) String name,
-        @Length(min = 3, max = 100) String supplierCode
+        @Schema(
+                name = "name",
+                description = "Nome do produto",
+                examples = {
+                        "Cola Quente para Tecido - Bastão 20cm",
+                        "Molde para Capas de Livro A5 - Coração",
+                        "Fita de Cetim 5cm - Vermelho Vinho"
+                }
+        )
+        @Length(min = 2, max = 100)
+        String name,
+
+        @Schema(
+                name = "supplierCode",
+                description = "Código do fornecedor do produto",
+                examples = {
+                        "CODIGO-001",
+                        "Dona Lúcia +551191234-5678",
+                        "Loja de Materiais Artísticos, Rua das Flores, 12345",
+                }
+        )
+        @Length(min = 3, max = 100)
+        String supplierCode
 ) {}
